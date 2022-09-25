826bfb2ebd903d2650c25d821d2dac1b5f55ffd7
fts <fts@gmail.com>
2022-09-26 00:58:08 +0200

    footer edit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="OJ" href="http://www.papareact.com/">
+                OJ
               </a>
             </li>
           </ul>
